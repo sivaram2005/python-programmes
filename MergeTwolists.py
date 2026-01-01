0fb0da041ea7cb8cc32a71f0b0264d25a7d21909
sivaram2005 <baadepallisivaramireddy@gmail.com>
2026-01-01 13:05:39 +0530

    Create MergeTwolists.py

---
+++ MergeTwolists.py
@@ -0,0 +1,5 @@
+a=list(map(int,input("enter elements in list 1:").split()))
+b=list(map(int,input("enter elements in list 2:").split()))
+print("before merging:",a,b)
+merge=a+b
+print("after merging two lists:",merge)
